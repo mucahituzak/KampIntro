10ced4e98dd1dff51e88e08ff005ae28be180c18
mucahituzak <58111060+mucahituzak@users.noreply.github.com>
2021-01-19 20:01:52 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Mücahit";
+            int yas = 20;
+
+            Kurs kurs1=new Kurs();
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Mucahit Uzak";
+            kurs1.IzlenmeOranı = 100;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.KursAdi = "C++";
+            kurs2.Egitmen = "Mehmet Uzak";
+            kurs2.IzlenmeOranı = 10;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.KursAdi = "Java";
+            kurs3.Egitmen = "Kerem";
+            kurs3.IzlenmeOranı = 789;
+
+
+
+
+
+
+            //Console.WriteLine(kurs1.KursAdi + " : " + kurs1.Egitmen);
+
+            Kurs[] kurslar = new Kurs[] {kurs1,kurs2,kurs3 };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + " : " + kurs.Egitmen);
+            }
+
+            
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+
+        public int IzlenmeOranı  { get; set; }
+    }
+}
---
+++ Odev2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Odev2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programlama Geliştirici Kampı");
+            Console.WriteLine("Hepsiburada");
+
+            Urun urun1 = new Urun();
+            urun1.UrunListesi = "Laptop";
+            urun1.UrunFiyati = 200;
+            urun1.Satici = "Mücahit ";
+
+            Urun urun2 = new Urun();
+            urun2.UrunListesi = "Kulaklık";
+            urun2.UrunFiyati = 20;
+            urun2.Satici = "Enes ";
+
+            Urun urun3 = new Urun();
+            urun3.UrunListesi = "Masa";
+            urun3.UrunFiyati = 200;
+            urun3.Satici = "Rahman ";
+
+
+            Urun urun4 = new Urun();
+            urun4.UrunListesi = "Telefon";
+            urun4.UrunFiyati = 2000 ;
+            urun4.Satici = "Eren ";
+            
+
+
+
+         Urun[] urunler = new Urun[] {urun1,urun2,urun3,urun4 };
+
+            Console.WriteLine("FOR DÖNGÜSÜ");
+            for (int i = 0; i <urunler.Length; i++)
+            {
+                Console.WriteLine("Ürünler= "+urunler[i].UrunListesi+ " : " + urunler[i].UrunFiyati+" tl "+" Satici: " + urunler[i].Satici);
+            }
+
+            Console.WriteLine("------------------------------------");
+
+            Console.WriteLine("WHİLE DÖNGÜSÜ");
+
+            int a = 0;
+            while (a < urunler.Length) 
+            {
+                Console.WriteLine("Ürünler= " + urunler[a].UrunListesi + " : " + urunler[a].UrunFiyati + " tl " + " Satici: " + urunler[a].Satici);
+                a++;
+            }
+
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("FOREACH DÖNGÜSÜ");
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine("Ürünler ="+urun.UrunListesi + " : " + urun.UrunFiyati+ " tl "+" Satıcı: "+urun.Satici);
+            }
+
+
+        }
+        class Urun
+        {
+            public string UrunListesi { get; set; }
+            public int UrunFiyati { get; set; }
+
+            public string Satici { get; set; }
+
+        }
+    }
+}
